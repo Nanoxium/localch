576b9e0737ec827cafd1ac115865f78d4388738c
Nanoxium <nanoxium@posmith.ch>
2015-11-18 12:58:25 +0100

    first commit with initial code

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Local.ch</title>
+        <link rel="stylesheet" href="./css/style.css" />
+        <script type="text/javascript" src="js/jquery-2.1.4.min.js" ></script>
+        <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+        <script type="text/javascript" src="js/script.js" ></script>
+    </head>
+    <body onload="initMap()">
+        <div id="content">
+            <table>
+                <tr>
+                    <td>Latitude : </td><td id="lat"></td>
+                </tr>
+                <tr>
+                    <td>Longitude : </td><td id="long"></td>
+                </tr>
+                <tr>
+                    <td>Précision : </td><td id="prec"></td>
+                </tr>
+                <tr>
+                    <td>Altitude : </td><td id="alt"></td>
+                </tr>
+                <tr>
+                    <td>Précision altitude : </td><td id="precalt"></td>
+                </tr>
+                <tr>
+                    <td>Angle par rapport au nord : </td><td id="angle"></td>
+                </tr>
+                <tr>
+                    <td>Vitesse de déplacement : </td><td id="speed"></td>
+                </tr>
+                <tr>
+                    <td>Temps : </td><td id="time"></td>
+                </tr>
+            </table><br/>
+            <button id="start" onclick="startWatch()">Commencer la géolocalisation</button>
+            <button id="stop" onclick="stopWatch()" disabled="">Arrêter la géolocalisation</button><br/>
+            <div id="mapDiv">
+            </div>
+        </div>
+    </body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,72 @@
+var map = null;
+var marker = null;
+var watchId = null;
+
+function startWatch() {
+    $("#stop").removeAttr('disabled');
+    $("#start").attr("disabled", "false");
+    if (navigator.geolocation)
+    {
+        watchId = navigator.geolocation.watchPosition(successCallback, errorCallback, {enableHighAccuracy: true, timeout: 10000, maximumAge: 0});
+    }
+    else
+    {
+        alert("Votre navigateur ne prend pas en compte la géolocalisation HTML5");
+    }
+}
+
+function initMap()
+{
+    var latLng = new google.maps.LatLng(0,0);
+    var mapOptions = {
+        zoom : 8,
+        center : latLng,
+        mapTypeId : google.maps.MapTypeId.ROADMAP
+    };
+    
+    map = new google.maps.Map(document.getElementById("mapDiv"), mapOptions);
+}
+
+function stopWatch() {
+    $("#stop").attr("disabled", "true");
+    $("#start").removeAttr("disabled");
+    navigator.geolocation.clearWatch(watchId);
+}
+
+function successCallback(position)
+{
+    marker = null;
+    $("#lat").text(position.coords.latitude);
+    $("#long").text(position.coords.longitude);
+    $("#prec").text(position.coords.accuracy);
+    $("#alt").text(position.coords.altitude);
+    $("#precalt").text(position.coords.altitudeAccuracy);
+    $("#angle").text(position.coords.heading);
+    $("#speed").text(position.coords.speed);
+    $("#time").text(new DateTime(position.timestamp));
+    var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    map.panTo(pos);
+    marker = new google.maps.Marker({
+        position : pos,
+        map : map
+    });
+}
+
+function errorCallback(error)
+{
+    switch (error.code)
+    {
+        case error.PERMISSION_DENIED:
+            alert("L'utilisateur n'a pas autorisé l'accès à sa position");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("L'emplacement de l'utilisateur n'a pas pu être déterminé");
+            break;
+        case error.TIMEOUT:
+            alert("Le service n'a pas répondu à temps");
+            break;
+        default:
+            alert("Erreur de géolocalisation");
+            break;
+    }
+}
